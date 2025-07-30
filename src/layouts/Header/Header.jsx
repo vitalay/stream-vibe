44db6a566b58538c7257df5ae4b0a7b7e28838ab
vitalya <vitalay.efimov@gmail.com>
2025-07-30 22:20:32 +0500

    Компонент Logo, библиотека classNames

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,9 +1,14 @@
 import './Header.scss'
-
+import Logo from '@/components/Logo'
 const Header = () => {
   return (
     <header className='header'>
-    HEADER
+    <div className="header__inner container">
+        <Logo
+          className="header__logo"
+          loading="eager"
+        />
+      </div>
     </header>
 );
 };
