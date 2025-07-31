8d3a550ccb8c9d9b4f0ee729fc3c210c259359a6
vitalya <vitalay.efimov@gmail.com>
2025-07-31 20:27:35 +0500

    Меню навигации, списки в JSX, метод массивов map

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,6 +1,28 @@
 import './Header.scss'
 import Logo from '@/components/Logo'
-const Header = () => {
+import classNames from 'classnames';
+const  Header = (props) => {
+ const {
+  url,
+ } = props
+  const menuItems = [
+    {
+      label: 'Home',
+      href: '/',
+    },
+    {
+      label: 'Movies & Shows',
+      href: '/movies',
+    },
+    {
+      label: 'Support',
+      href: '/support',
+    },
+    {
+      label: 'Subscriptions',
+      href: '/subscriptions',
+    },
+  ]
   return (
     <header className='header'>
     <div className="header__inner container">
@@ -8,6 +30,22 @@ const Header = () => {
           className="header__logo"
           loading="eager"
         />
+        <nav className="header__menu">
+          <ul className="header__menu-list">
+                {menuItems.map(({ label, href }, index) => (
+                <li className="header__menu-item" key={index}>
+                  <a
+                    className={classNames('header__menu-link', {
+                      'is-active': href === url
+                    })}
+                    href={href}
+                  >
+                    {label}
+                  </a>
+                </li>
+              ))}
+          </ul>
+        </nav>
       </div>
     </header>
 );
